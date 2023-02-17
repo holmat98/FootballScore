b9df484e973b4b02cf9eb886bec37c7d2d740a90
holmat98 <holmat1998@gmail.com>
2023-02-17 18:10:55 +0100

    Added domain module

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id(Plugins.ANDROID_LIBRARY)
+    id(Plugins.KOTLIN_ANDROID)
+    kotlin (Plugins.KAPT)
+    id (Plugins.HILT)
+}
+
+android {
+    namespace = "com.mateuszholik.domain"
+    compileSdk = DefaultConfig.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = DefaultConfig.MIN_SDK
+        targetSdk = DefaultConfig.TARGET_SDK
+
+        testInstrumentationRunner = DefaultConfig.TEST_INSTRUMENTATION_RUNNER
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                getDefaultProguardFile(Proguard.FILE),
+                Proguard.RULES_FILE
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    // Modules
+    implementation(project(":core:model"))
+    implementation(project(":core:data"))
+
+    // CoreKtx
+    implementation(CoreKtx.DEPENDENCY)
+
+    // Hilt
+    implementation(Hilt.DEPENDENCY)
+    kapt(Hilt.Compiler.DEPENDENCY)
+}
