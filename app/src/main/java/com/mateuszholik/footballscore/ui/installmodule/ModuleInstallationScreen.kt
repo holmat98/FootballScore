b5622e57dd3240c6be7d7af844040bab72bd896b
holmat98 <holmat1998@gmail.com>
2023-04-12 18:42:59 +0200

    12: removed unused import

--- app/src/main/java/com/mateuszholik/footballscore/ui/installmodule/ModuleInstallationScreen.kt
+++ app/src/main/java/com/mateuszholik/footballscore/ui/installmodule/ModuleInstallationScreen.kt
@@ -1,6 +1,5 @@
 package com.mateuszholik.footballscore.ui.installmodule
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
