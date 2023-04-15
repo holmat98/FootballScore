f61aa4f8c0d17be5335c824fbd69a95d15512e31
holmat98 <holmat1998@gmail.com>
2023-04-15 14:00:22 +0200

    15: created GetCombinedCompetitionDetailsUseCase

---
+++ core/domain/src/main/java/com/mateuszholik/domain/usecases/GetCombinedCompetitionDetailsUseCase.kt
@@ -0,0 +1,63 @@
+package com.mateuszholik.domain.usecases
+
+import com.mateuszholik.data.repositories.CompetitionRepository
+import com.mateuszholik.domain.usecases.base.ParameterizedFlowUseCase
+import com.mateuszholik.model.CombinedCompetitionDetails
+import com.mateuszholik.model.CompetitionDetails
+import com.mateuszholik.model.CompetitionStandingsDetails
+import com.mateuszholik.model.ErrorType
+import com.mateuszholik.model.Result
+import com.mateuszholik.model.Scorer
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+
+interface GetCombinedCompetitionDetailsUseCase :
+    ParameterizedFlowUseCase<Int, CombinedCompetitionDetails>
+
+internal class GetCombinedCompetitionDetailsUseCaseImpl(
+    private val competitionRepository: CompetitionRepository,
+) : GetCombinedCompetitionDetailsUseCase {
+
+    override fun invoke(param: Int): Flow<Result<CombinedCompetitionDetails>> =
+        combine(
+            competitionRepository.getCompetition(param),
+            competitionRepository.getCompetitionStandings(param),
+            competitionRepository.getCompetitionTopScorers(param)
+        ) { competitionResult, competitionStandingsResult, topScorersResult ->
+
+            when {
+                competitionResult is Result.Success &&
+                competitionStandingsResult is Result.Success &&
+                topScorersResult is Result.Success ->
+                    createCombinedCompetitionDetailsResult(
+                        competitionResult.data,
+                        competitionStandingsResult.data,
+                        topScorersResult.data
+                    )
+                competitionResult is Result.Error -> Result.Error(competitionResult.errorType)
+                competitionStandingsResult is Result.Error -> Result.Error(
+                    competitionStandingsResult.errorType
+                )
+                topScorersResult is Result.Error -> Result.Error(topScorersResult.errorType)
+                else -> Result.Error(ErrorType.UNKNOWN)
+            }
+        }
+
+    private fun createCombinedCompetitionDetailsResult(
+        competitionDetails: CompetitionDetails,
+        competitionStandingsDetails: List<CompetitionStandingsDetails>,
+        scorers: List<Scorer>,
+    ): Result<CombinedCompetitionDetails> =
+        Result.Success(
+            CombinedCompetitionDetails(
+                area = competitionDetails.area,
+                id = competitionDetails.id,
+                name = competitionDetails.name,
+                code = competitionDetails.code,
+                type = competitionDetails.type,
+                emblem = competitionDetails.emblem,
+                standingsDetails = competitionStandingsDetails,
+                topScorers = scorers
+            )
+        )
+}
