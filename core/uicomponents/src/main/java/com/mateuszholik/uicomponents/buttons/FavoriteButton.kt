a444e07fb102f4bcfb0c99b5570f21c3007649fc
holmat98 <holmat1998@gmail.com>
2023-05-30 18:35:14 +0200

    Edited FavoriteButton composable

--- core/uicomponents/src/main/java/com/mateuszholik/uicomponents/buttons/FavoriteButton.kt
+++ core/uicomponents/src/main/java/com/mateuszholik/uicomponents/buttons/FavoriteButton.kt
@@ -10,10 +10,6 @@ import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,18 +22,13 @@ import com.mateuszholik.designsystem.theme.spacing
 fun FavoriteButton(
     onClick: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
-    initialState: Boolean = false,
+    isChecked: Boolean = false,
     tint: Color = Color.Yellow,
 ) {
-    var isChecked by remember { mutableStateOf(initialState) }
-
     Box(
         modifier = modifier
             .clip(CircleShape)
-            .clickable {
-                isChecked = isChecked.not()
-                onClick(isChecked)
-            },
+            .clickable { onClick(!isChecked) },
         contentAlignment = Alignment.Center
     ) {
         Icon(
@@ -57,6 +48,6 @@ fun FavoriteButton(
 @Composable
 private fun Preview() {
     FootballScoreTheme {
-        FavoriteButton(initialState = false, onClick = {})
+        FavoriteButton(isChecked = false, onClick = {})
     }
 }
