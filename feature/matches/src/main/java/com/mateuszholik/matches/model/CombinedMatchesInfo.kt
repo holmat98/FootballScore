99affb2ec3435bfc4d6b5a0ed9f153fea4b6f369
holmat98 <holmat1998@gmail.com>
2023-05-31 18:22:55 +0200

    22: Removed unused model

--- feature/matches/src/main/java/com/mateuszholik/matches/model/CombinedMatchesInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.mateuszholik.matches.model
-
-import com.mateuszholik.model.Competition
-import com.mateuszholik.model.MatchInfo
-
-data class CombinedMatchesInfo(
-    val matches: Map<Competition, List<MatchInfo>>,
-    val watchedMatchesIds: List<Int> = emptyList(),
-)
